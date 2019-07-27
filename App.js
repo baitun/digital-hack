fee818a69aebde0ceb1ed37d164ca7300340e061
Yury Savin <yury@savin.dev>
2019-07-28 00:32:09 +0300

    ➕ react-native-vector-icons

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { Button } from 'react-native-material-ui';
 
 class HomeScreen extends React.Component {
   render() {
@@ -8,7 +9,9 @@ class HomeScreen extends React.Component {
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Home Screen</Text>
         <Button
-          title="Go to Details"
+          raised
+          primary
+          text="Go to Details"
           onPress={() => this.props.navigation.navigate('Details')}
         />
       </View>
@@ -22,7 +25,7 @@ class DetailsScreen extends React.Component {
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Details Screen</Text>
         <Button
-          title="Go to Details... again"
+          text="Go to Details... again"
           onPress={() => this.props.navigation.navigate('Details')}
         />
       </View>
@@ -32,8 +35,8 @@ class DetailsScreen extends React.Component {
 
 const AppNavigator = createStackNavigator(
   {
-    Home: HomeScreen,
-    Details: DetailsScreen,
+    Home: { screen: HomeScreen },
+    Details: { screen: DetailsScreen },
   },
   {
     initialRouteName: 'Home',
